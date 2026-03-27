0a841d9e73884a04f5b9909143d90c5201de52ae
T0ks1k24 <toksamal2004@gmail.com>
2026-03-27 11:33:08 +0200

    fead() make license and update setup, pyproject

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='ecp-lib',
-    version="0.1.0",
-    packages=find_packages(include=["ecp_lib", "ecp_lib.*"]),
-    install_requires=[
-        'cryptography>=46.0.6',
-    ],
-    extras_require={
-        "django": [
-            'django>=4.2,<6.0',
-        ],
-    },
-)
+
+setup()
